ddedee807448d280754f30f3e45e3e8836bb34d5
Cecil Chris - Esema (MEA) <Chris.Esema@dimensiondata.com>
2020-06-08 08:29:51 +0200

    Updates

---
+++ precache-manifest.4c14110afa13ba5004e21e784ea38d9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f33a8873e64652f5ee7de8d69dcd824",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "a7b2603b2252d5751188",
+    "url": "/profile/static/css/main.14d54006.chunk.css"
+  },
+  {
+    "revision": "d2a3f3e710ec11f8cbd6",
+    "url": "/profile/static/js/2.42a726c8.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/profile/static/js/2.42a726c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b2603b2252d5751188",
+    "url": "/profile/static/js/main.cf730df0.chunk.js"
+  },
+  {
+    "revision": "7aee09b010bdcab3317d",
+    "url": "/profile/static/js/runtime-main.35e615fd.js"
+  }
+]);
